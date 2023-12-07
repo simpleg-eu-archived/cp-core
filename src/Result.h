f24215e33586dde4128ac6b03cd45e69bbc7c998
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-12-07 15:28:36 +0100

    r reformat code

--- src/Result.h
+++ src/Result.h
@@ -3,53 +3,56 @@
 #include <memory>
 
 namespace cuplan::core {
-  template <typename TOk, typename TError>
-  class Result {
-  public:
-    static Result Ok(std::unique_ptr<TOk> ok) {
-      return Result(std::move(ok), nullptr, true);
+template <typename TOk, typename TError>
+class Result {
+ public:
+  static Result Ok(std::unique_ptr<TOk> ok) {
+    return Result(std::move(ok), nullptr, true);
+  }
+
+  static Result Err(std::unique_ptr<TError> error) {
+    return Result(nullptr, std::move(error), false);
+  }
+
+  TOk Unwrap() const {
+    if (!success_) {
+      throw std::logic_error(
+          "Tried to unwrap 'ok' value when result is an 'error'.");
     }
 
-    static Result Err(std::unique_ptr<TError> error) {
-      return Result(nullptr, std::move(error), false);
+    return *ok_;
+  }
+
+  TError UnwrapErr() const {
+    if (success_) {
+      throw std::logic_error(
+          "Tried to unwrap 'error' value when result is an 'ok'.");
     }
 
-    TOk Unwrap() const {
-      if (!success_) {
-        throw std::logic_error("Tried to unwrap 'ok' value when result is an 'error'.");
-      }
+    return *err_;
+  }
 
-      return *ok_;
+ private:
+  std::shared_ptr<TOk> ok_;
+  std::shared_ptr<TError> err_;
+
+  bool success_;
+
+  Result(std::unique_ptr<TOk> ok, std::unique_ptr<TError> error, bool success) {
+    if (ok != nullptr) {
+      ok_ = std::move(ok);
+    } else {
+      ok_ = nullptr;
     }
 
-    TError UnwrapErr() const {
-      if (success_) {
-        throw std::logic_error("Tried to unwrap 'error' value when result is an 'ok'.");
-      }
-
-      return *err_;
+    if (error != nullptr) {
+      err_ = std::move(error);
+    } else {
+      err_ = nullptr;
     }
-  private:
-    std::shared_ptr<TOk> ok_;
-    std::shared_ptr<TError> err_;
 
-    bool success_;
-
-    Result(std::unique_ptr<TOk> ok, std::unique_ptr<TError> error, bool success) {
-      if (ok != nullptr) {
-        ok_ = std::move(ok);
-      } else {
-        ok_ = nullptr;
-      }
-
-      if (error != nullptr) {
-        err_ = std::move(error);
-      } else {
-        err_ = nullptr;
-      }
-
-      success_ = success;
-    }
-  };
-}
-#endif //RESULT_H
+    success_ = success;
+  }
+};
+}  // namespace cuplan::core
+#endif  // RESULT_H
